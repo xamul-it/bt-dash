938db6e1b31cd2f1d47d651408e33925e163570d
Luigi <luigi.zanderighi@gmail.com>
2022-03-16 16:51:59 +0100

    fix path

--- src/boot/constants.js
+++ src/boot/constants.js
@@ -1,8 +1,8 @@
 
 const constants = {
-  API_BASE_URL: 'http://localhost:8080',
-  API_BASE_FOLDER: '/out',
-  API_BACKUP_FOLDER: '/backup',
+  API_BASE_URL: 'http://213.171.162.127/dash',
+  API_BASE_FOLDER: '../out',
+  API_BACKUP_FOLDER: '../backup',
   API_INDEX_FILE: '/index.json',
   API_TICKER_DATA_FILE: '/results.json',
   STATS_FILE: '/stats.html',
